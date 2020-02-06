8bda3fd6dd0b1744257f601dab346af954cea879
seba grabowski <seba.grabowski@hotmail.com>
2020-02-06 17:31:54 +0100

    ładne ify

--- sterowanie programem.py
+++ sterowanie programem.py
@@ -5,7 +5,7 @@ if age >=18:
 else:
     print("you are too young to buy alcohol")
 
-isDrunk= True
+isDrunk= False
 if age >=18 and not isDrunk:
     print("You are adult and you can buy alcohol")
 else:
@@ -17,4 +17,13 @@ if age >=18 and not isDrunk and not isRestrictedArea:
     print("You are adult and you can buy alcohol")
 else:
     print("you are too young to buy alcohol")
+
+if age <18:
+    print("too young")
+elif isDrunk:
+    print("are you drank")
+elif isRestrictedArea:
+    print("nie wolno tu")
+else:
+    print("kup se")
 
